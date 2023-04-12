9aec2863993f775a00a1dde69260045131768e47
Mariana Harris <marianaharris94@gmail.com>
2023-04-11 19:12:21 -0700

    updates

--- run_SoftSweeps_1000.sh
+++ run_SoftSweeps_1000.sh
@@ -16,10 +16,6 @@ for trial in {1..100};do
         #######################No NA haplotypes############################################$
         ##################################################################################$
 
-	## Mesolithic sample
-        python H12_H2H1_MD.py tmp/out_selec_mesolithic_MD.txt 177 -o tmp/out_H12_mesolithic_NoNAhap.txt -w 200 -j 1 -d 0
-        sed -e 'y/\t/\n/'  tmp/out_H12_mesolithic_NoNAhap.txt | sed -n '9p' > tmp/H12_mesolithic_NoNAhap.txt
-        sed 's/.*/&\t '"$K"'/' tmp/H12_mesolithic_NoNAhap.txt >> out/out_H12_mesolithic_NoNAhap.txt
 
 	##250
  	python H12_H2H1_MD.py tmp/out_selec_250_MD.txt 177 -o tmp/out_H12_250_NoNAhap.txt -w 200 -j 1 -d 0
@@ -31,7 +27,7 @@ for trial in {1..100};do
         sed -e 'y/\t/\n/'  tmp/out_H12_100_NoNAhap.txt | sed -n '9p' > tmp/H12_100_NoNAhap.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_100_NoNAhap.txt >> out/out_H12_100_NoNAhap.txt
 
-	## Modern
+	## 40
         python H12_H2H1_MD.py tmp/out_selec_modern_MD.txt 177 -o tmp/out_H12_modern_NoNAhap.txt -w 200 -j 1 -d 0
         sed -e 'y/\t/\n/'  tmp/out_H12_modern_NoNAhap.txt | sed -n '9p' > tmp/H12_modern_NoNAhap.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_modern_NoNAhap.txt >> out/out_H12_modern_NoNAhap.txt
@@ -45,11 +41,7 @@ for trial in {1..100};do
 	#    python H12_H2H1_Py3.py tmp/out_selec_modern_MD.txt 187 -o H12_tmp_mod/out_H12_modern_${i}.txt -w 100
 	#done
 
-	#Merge H12 files and get mean H12 of 100 runs
-	#L_meso=$(wc -l H12_tmp_meso/out_H12_mesolithic_1.txt)
-	#numL_meso=${L_meso%% *}
-	#python H12_MD.py -d H12_tmp_meso/ -l ${numL_meso} -f tmp/H12_merged_mesolithic.txt
-
+	#Merge H12 files and get mean H12
 	#L_modern=$(wc -l H12_tmp_mod/out_H12_modern_1.txt)
 	#numL_modern=${L_modern%% *}
 	#python H12_MD.py -d H12_tmp_mod/ -l ${numL_modern} -f tmp/H12_merged_modern.txt
@@ -65,10 +57,6 @@ for trial in {1..100};do
 	#######################No missing data################################################
 	######################################################################################
         
-	## Mesolithic sample
-        python H12_H2H1_Py3.py tmp/out_selec_mesolithic.txt 177 -o tmp/out_H12_mesolithic.txt -w 200 -j 1 -d 0
-        sed -e 'y/\t/\n/'  tmp/out_H12_mesolithic.txt | sed -n '9p' > tmp/H12_mesolithicFull.txt
-        sed 's/.*/&\t '"$K"'/' tmp/H12_mesolithicFull.txt >> out/out_H12_mesolithic.txt
 
 	##250
  	python H12_H2H1_Py3.py tmp/out_selec_250.txt 177 -o tmp/out_H12_250.txt -w 200 -j 1 -d 0
@@ -80,21 +68,18 @@ for trial in {1..100};do
         sed -e 'y/\t/\n/'  tmp/out_H12_100.txt | sed -n '9p' > tmp/H12_100Full.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_100Full.txt >> out/out_H12_100.txt
 
-	## Modern
+	## 40
         python H12_H2H1_Py3.py tmp/out_selec_modern.txt 177 -o tmp/out_H12_modern.txt -w 200 -j 1 -d 0
         sed -e 'y/\t/\n/'  tmp/out_H12_modern.txt | sed -n '9p' > tmp/H12_modernFull.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_modernFull.txt >> out/out_H12_modern.txt
 
 
-         ######################################################################################
+        ######################################################################################
 	#######################normal H12 with missing data##################################
 	######################################################################################
 
-        python H12_H2H1_Py3.py tmp/out_selec_mesolithic_MD.txt 177 -o tmp/out_H12_mesolithic_MD.txt -w 200 -j 1 -d 0
-        sed -e 'y/\t/\n/'  tmp/out_H12_mesolithic_MD.txt | sed -n '9p' > tmp/H12_mesolithic_MD.txt
-        sed 's/.*/&\t '"$K"'/' tmp/H12_mesolithic_MD.txt >> out/out_H12_mesolithic_MD.txt
 
- 	 ##250
+ 	##250
 	python H12_H2H1_Py3.py tmp/out_selec_250_MD.txt 177 -o tmp/out_H12_250_MD.txt  -w 200 -j 1 -d 0
         sed -e 'y/\t/\n/'  tmp/out_H12_250_MD.txt | sed -n '9p' > tmp/H12_250_MD.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_250_MD.txt >> out/out_H12_250_MD.txt
@@ -103,15 +88,15 @@ for trial in {1..100};do
 	python H12_H2H1_Py3.py tmp/out_selec_100_MD.txt 177 -o tmp/out_H12_100_MD.txt  -w 200 -j 1 -d 0
         sed -e 'y/\t/\n/'  tmp/out_H12_100_MD.txt | sed -n '9p' > tmp/H12_100_MD.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_100_MD.txt >> out/out_H12_100_MD.txt
-
+	
+	##40
         python H12_H2H1_Py3.py tmp/out_selec_modern_MD.txt 177 -o tmp/out_H12_modern_MD.txt -w 200 -j 1 -d 0
         sed -e 'y/\t/\n/'  tmp/out_H12_modern_MD.txt | sed -n '9p' > tmp/H12_modern_MD.txt
         sed 's/.*/&\t '"$K"'/' tmp/H12_modern_MD.txt >> out/out_H12_modern_MD.txt
 
 	# delete tmp files
 	rm  tmp/*
-	#rm  H12_tmp_meso/*
-	#rm H12_tmp_mod/*
+	
 
       done
 done
